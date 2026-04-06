ef33819b9f2aa18f02c43ad2ba22704b24eabbd5
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2026-04-06 16:53:05 +0800

    feat(desktop): Add more MIME apps

--- modules/desktop/environment/mimeapps.nix
+++ modules/desktop/environment/mimeapps.nix
@@ -8,9 +8,16 @@
           enable = true;
           defaultApplicationPackages = [
             config.programs.mpv.package
+            config.programs.neovide.package
             pkgs.file-roller
           ];
+          associations = {
+            added = {
+              "image/*" = "firefox.desktop";
+            };
+          };
           defaultApplications = {
+            "image/*" = "firefox.desktop";
             "text/html" = "firefox.desktop";
             "x-scheme-handler/http" = "firefox.desktop";
             "x-scheme-handler/https" = "firefox.desktop";
