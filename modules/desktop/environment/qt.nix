134d390605c5b23238d2919ef2ca42350fc3c2af
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2026-06-01 12:31:21 +0800

    desktop/qt: Use Kvantum

--- modules/desktop/environment/qt.nix
+++ modules/desktop/environment/qt.nix
@@ -6,7 +6,7 @@
         let
           qtctSettings = {
             Appearance = {
-              style = "Fusion";
+              style = "kvantum";
               # Sync with GTK
               icon_theme = config.gtk.iconTheme.name;
               standard_dialogs = "xdgdesktopportal";
@@ -20,6 +20,8 @@
         lib.mkIf (config.qt.platformTheme.name == "qtct") {
           qt5ctSettings = qtctSettings;
           qt6ctSettings = qtctSettings;
+
+          style.name = "kvantum";
         };
     };
 }
