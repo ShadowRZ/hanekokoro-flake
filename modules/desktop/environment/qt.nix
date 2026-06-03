700329891f0c0f0537e09ce2885c367999dc3587
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2026-06-03 13:53:35 +0800

    nixos/desktop/qt: Actually enable Kvantum

--- modules/desktop/environment/qt.nix
+++ modules/desktop/environment/qt.nix
@@ -1,25 +1,45 @@
 {
   flake.modules.homeManager.desktop =
-    { config, lib, ... }:
     {
-      qt =
-        let
-          qtctSettings = {
-            Appearance = {
-              style = "kvantum";
-              # Sync with GTK
-              icon_theme = config.gtk.iconTheme.name;
-              standard_dialogs = "xdgdesktopportal";
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
+    {
+      qt = lib.mkMerge [
+        (
+          let
+            qtctSettings = {
+              Appearance = {
+                style = "kvantum";
+                # Sync with GTK
+                icon_theme = config.gtk.iconTheme.name;
+                standard_dialogs = "xdgdesktopportal";
+              };
+              Fonts = {
+                fixed = "\"Hanekokoro Mono,12,-1,5,50,0,0,0,0,0,Extended\"";
+                general = "\"Space Grotesk,12\"";
+              };
             };
-            Fonts = {
-              fixed = "\"Hanekokoro Mono,12,-1,5,50,0,0,0,0,0,Extended\"";
-              general = "\"Space Grotesk,12\"";
+          in
+          lib.mkIf (config.qt.platformTheme.name == "qtct") {
+            qt5ctSettings = qtctSettings;
+            qt6ctSettings = qtctSettings;
+          }
+        )
+        {
+          kvantum = {
+            enable = true;
+            settings = {
+              General.theme = "catppuccin-latte-rosewater";
             };
           };
-        in
-        lib.mkIf (config.qt.platformTheme.name == "qtct") {
-          qt5ctSettings = qtctSettings;
-          qt6ctSettings = qtctSettings;
-        };
+        }
+      ];
+
+      home.packages = [
+        pkgs.qt6Packages.qtstyleplugin-kvantum
+      ];
     };
 }
