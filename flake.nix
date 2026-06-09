c3d730badc40e423a64e12c1593f75d3857f1228
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2026-06-09 15:14:59 +0800

    flake.nix: Update noctalia-shell input

--- flake.nix
+++ flake.nix
@@ -97,7 +97,7 @@
       };
     };
     noctalia-shell = {
-      url = "github:noctalia-dev/noctalia-shell/v5";
+      url = "github:noctalia-dev/noctalia-shell";
       inputs = {
         nixpkgs.follows = "nixpkgs";
       };
