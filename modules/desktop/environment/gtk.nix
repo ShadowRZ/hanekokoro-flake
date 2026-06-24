50bdd3cde68b9e8dc329538c7869eecb13734ab3
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2026-06-24 09:28:54 +0800

    nixos/gtk: Remove GTK2

--- modules/desktop/environment/gtk.nix
+++ modules/desktop/environment/gtk.nix
@@ -1,7 +1,7 @@
 {
   flake.modules.homeManager = {
     desktop =
-      { config, ... }:
+      _:
       {
         gtk =
           let
@@ -18,25 +18,12 @@
               name = "Space Grotesk";
               size = 13;
             };
-            gtk2 = {
-              configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
-              extraConfig = ''
-                gtk-can-change-accels = 1
-                gtk-enable-animations = 1
-                gtk-primary-button-warps-slider = 1
-                gtk-toolbar-style = 3
-                gtk-menu-images = 1
-                gtk-button-images = 1
-              '';
-            };
             gtk3.extraConfig = gtkIni;
             gtk4.extraConfig = gtkIni;
             theme = {
               name = "adw-gtk3";
             };
           };
-
-        systemd.user.sessionVariables.GTK2_RC_FILES = config.home.sessionVariables.GTK2_RC_FILES;
 
         dconf.settings = {
           "org/gnome/desktop/interface" = {
