ae1122b3a8f8a7b62bcbabae0cea55d25225300d
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2026-07-10 09:07:19 +0800

    nixos/desktop: explictly enable Home Manager cursor management

--- modules/desktop/environment/cursor.nix
+++ modules/desktop/environment/cursor.nix
@@ -1,6 +1,7 @@
 {
   flake.modules.homeManager.desktop = { pkgs, ... }: {
     home.pointerCursor = {
+      enable = true;
       package = pkgs.bibata-cursors;
       name = "Bibata-Modern-Ice";
       size = 32;
