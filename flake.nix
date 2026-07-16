86754c8d63d013bbb42a4eb5a963fca68e9c7fbe
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2026-07-16 08:18:54 +0800

    treewide: Reformat

--- flake.nix
+++ flake.nix
@@ -129,9 +129,9 @@
     ];
     extra-trusted-public-keys = [
       # keep-sorted start
+      "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "shadowrz.cachix.org-1:I+6FCWMtdGmN8zYVncKdys/LVsLkCMWO3tfXbwQPTU0="
-      "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
       # keep-sorted end
     ];
     extra-experimental-features = [
