43987538ec29bd098b41ee4dfab8ddd291460b9f
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2026-07-27 22:29:26 +0800

    nixos/niri: Fix build

--- modules/desktop/session/niri.nix
+++ modules/desktop/session/niri.nix
@@ -50,6 +50,7 @@
 
       programs = {
         niri.enable = true;
+        niri.package = pkgs.niri.override { libdisplay-info = pkgs.libdisplay-info_0_3; };
         nm-applet.enable = true;
         silentSDDM = {
           enable = true;
