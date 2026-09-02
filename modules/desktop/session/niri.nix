7c4a1b06f60ede72d18b23d6e9d546242bacbbf8
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2026-09-03 07:16:18 +0800

    nixos/niri: Use Noctalia Greeter

--- modules/desktop/session/niri.nix
+++ modules/desktop/session/niri.nix
@@ -1,29 +1,31 @@
-{ inputs, ... }:
 {
   flake.modules.nixos."desktop/niri" =
     {
-      config,
       pkgs,
       lib,
       ...
     }:
     {
-
-      imports = [ inputs.silent-sddm.nixosModules.default ];
-
       services = {
         displayManager = {
           # SDDM
-          sddm = {
+          noctalia-greeter = {
+            cursorTheme = {
+              name = "Bibata-Modern-Ice";
+              package = pkgs.bibata-cursors;
+            };
             enable = true;
-            enableHidpi = true;
             settings = {
-              Theme = {
-                CursorTheme = "Bibata-Modern-Ice";
-                CursorSize = 32;
+              cursor = {
+                size = 32;
+                name = "Bibata-Modern-Ice";
               };
-              General = {
-                GreeterEnvironment = lib.mkForce "QML2_IMPORT_PATH=${config.programs.silentSDDM.package'}/share/sddm/themes/silent/components/,QT_IM_MODULE=qtvirtualkeyboard";
+              keyboard = {
+                layout = "us";
+              };
+              appearance = {
+                font_family = "Space Grotesk";
+                hide_logo = true;
               };
             };
           };
@@ -51,59 +53,6 @@
       programs = {
         niri.enable = true;
         nm-applet.enable = true;
-        silentSDDM = {
-          enable = true;
-          theme = "silvia";
-          backgrounds = {
-            nix-wallpaper-nineish = pkgs.nixos-artwork.wallpapers.nineish.src;
-          };
-          settings = {
-            "LoginScreen" = {
-              blur = 75;
-              background = "${pkgs.nixos-artwork.wallpapers.nineish.src.name}";
-            };
-            "LockScreen" = {
-              blur = 0;
-              background = "${pkgs.nixos-artwork.wallpapers.nineish.src.name}";
-            };
-            "LockScreen.Clock" = {
-              font-family = "Space Grotesk";
-            };
-            "LockScreen.Date" = {
-              font-family = "Space Grotesk";
-            };
-            "LockScreen.Message" = {
-              font-family = "Space Grotesk";
-            };
-            "LoginScreen.LoginArea.Username" = {
-              font-family = "Space Grotesk";
-            };
-            "LoginScreen.LoginArea.PasswordInput" = {
-              font-family = "Space Grotesk";
-            };
-            "LoginScreen.LoginArea.LoginButton" = {
-              font-family = "Space Grotesk";
-            };
-            "LoginScreen.LoginArea.Spinner" = {
-              font-family = "Space Grotesk";
-            };
-            "LoginScreen.LoginArea.WarningMessage" = {
-              font-family = "Space Grotesk";
-            };
-            "LoginScreen.MenuArea.Buttons" = {
-              font-family = "Space Grotesk";
-            };
-            "LoginScreen.MenuArea.Popups" = {
-              font-family = "Space Grotesk";
-            };
-            "Tooltips" = {
-              font-family = "Space Grotesk";
-            };
-            "LoginScreen.MenuArea.Layout" = {
-              display = false;
-            };
-          };
-        };
       };
 
       nix.settings = {
