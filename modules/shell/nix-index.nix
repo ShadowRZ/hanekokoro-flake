afdf71faf46b7f4a90bed616ac2545c23dee03d9
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2026-09-19 09:38:27 +0800

    nixos/shell/nix-index: Move to Home Manager
    Home Manager have Nushell integration

--- modules/shell/nix-index.nix
+++ modules/shell/nix-index.nix
@@ -3,13 +3,16 @@
     { pkgs, ... }:
     {
       environment.systemPackages = [ pkgs.comma-with-db ];
+    };
 
+  flake.modules.homeManager.shell =
+    { pkgs, ... }:
+    {
       programs = {
         # Nix-Index
         nix-index = {
           enable = true;
           package = pkgs.nix-index-with-db;
-          enableFishIntegration = true;
         };
       };
     };
